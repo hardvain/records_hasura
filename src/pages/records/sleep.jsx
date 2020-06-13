16c5b9d81b2922fa0bd7775ccb2bfd1e071fb296
Aravindh Sridharan <mail@aravindh.io>
2020-06-13 13:27:49 +0530

    Add pages for new record types

---
+++ src/pages/records/sleep.jsx
@@ -0,0 +1,46 @@
+import { Box, SimpleGrid, Stack } from '@chakra-ui/core';
+import { useEffect } from 'react';
+import Filters from 'src/components/records/Filters';
+import RecordsWithForm from 'src/components/records/RecordsWithForm';
+import DailyTrends from 'src/components/records/charts/DailyTrends';
+import WeeklyTrends from 'src/components/records/charts/WeeklyTrends';
+import { useStore } from 'src/store';
+
+export default () => {
+  const { date, setColors } = useStore((state) => ({
+    date: state.ui.date,
+    setColors: state.setColors,
+  }));
+
+  useEffect(() => {
+    setColors({ primary: 'blue', secondary: 'cyan' });
+  }, []);
+  return (
+    <Box py={30}>
+      <Filters
+        filters={{
+          recordType: 'sleep',
+          orderBy: 'timestamp',
+          orderDirection: 'asc',
+          date,
+        }}
+      >
+        {(filters) => (
+          <>
+            <RecordsWithForm
+              filters={filters}
+              frozenType={'sleep'}
+              collapseList={true}
+            />
+            <Box mt={4}>
+              <SimpleGrid columns={2} spacing={10}>
+                <DailyTrends filters={filters} />
+                <WeeklyTrends filters={filters} />
+              </SimpleGrid>
+            </Box>
+          </>
+        )}
+      </Filters>
+    </Box>
+  );
+};
