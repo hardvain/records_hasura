b611472b6f61412b69974dc412eb1bf4f3b7e3d9
Aravindh Sridharan <mail@aravindh.io>
2020-06-23 12:20:03 +0530

    Generic filters v1 completed

--- src/components/collection/Filters/index.jsx
+++ src/components/collection/Filters/index.jsx
@@ -8,7 +8,7 @@ import {
   RadioButtonGroup,
   IconButton,
 } from '@chakra-ui/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import FieldFilter from './FieldFilter';
 import Card from 'src/components/Card';
@@ -27,41 +27,84 @@ const CustomRadio = React.forwardRef((props, ref) => {
     />
   );
 });
-const FilterGroup = ({ fields, filterGroup, setFilterGroup, onDelete }) => {
-  const [filters, setFilters] = useState([]);
+const FilterGroup = ({
+  fields,
+  onDelete,
+  filter,
+  setFilter,
+  isRoot = false,
+}) => {
+  const [isNot, setIsNot] = useState(filter.isNot);
+  const [groupType, setGroupType] = useState(filter.groupType);
+
+  useEffect(() => {
+    setFilter({ ...filter, isNot });
+  }, [isNot]);
+
+  useEffect(() => {
+    setFilter({ ...filter, groupType });
+  }, [groupType]);
+
   const addFilter = () => {
-    setFilters([...filters, { type: 'field' }]);
+    setFilter({ ...filter, filters: [...filter.filters, { type: 'field' }] });
   };
   const addGroup = () => {
-    setFilters([...filters, { type: 'group' }]);
+    setFilter({
+      ...filter,
+      filters: [
+        ...filter.filters,
+        { type: 'group', groupType: '_and', isNot: false, filters: [] },
+      ],
+    });
   };
   const deleteFilter = (index) => {
-    setFilters(filters.filter((f, i) => i !== index));
+    setFilter({
+      ...filter,
+      filters: [filter.filters.filter((f, i) => i !== index)],
+    });
   };
-  const setFilter = (index) => (content) => {
-    const updatedFilters = [...filters];
-    updatedFilters[index] = content;
-    setFilters(updatedFilters);
-  };
+
   const deleteFilterGroup = (index) => {
-    setFilters(filters.filter((f, i) => i !== index));
+    setFilter({
+      ...filter,
+      filters: [filter.filters.filter((f, i) => i !== index)],
+    });
   };
-  const setChildFilterGroup = (index) => (content) => {
-    const updatedFilters = [...filters];
-    updatedFilters[index] = content;
-    setFilters(updatedFilters);
+
+  const setFilterField = (index) => (f) => {
+    const updatedFilters = [...filter.filters];
+    updatedFilters[index] = f;
+    setFilter({
+      ...filter,
+      filters: updatedFilters,
+    });
   };
+
+  const setFilterGroup = (index) => (f) => {
+    const updatedFilters = [...filter.filters];
+    updatedFilters[index] = f;
+    setFilter({
+      ...filter,
+      filters: updatedFilters,
+    });
+  };
+
   return (
     <Card highlight shadow>
       <Stack isInline my={3}>
-        <Checkbox>Not</Checkbox>
+        <Checkbox
+          isChecked={isNot}
+          onChange={(e) => setIsNot(e.target.checked)}
+        >
+          Not
+        </Checkbox>
         <RadioButtonGroup
-          defaultValue="and"
-          onChange={(val) => console.log(val)}
+          value={groupType}
+          onChange={(v) => setGroupType(v)}
           isInline
         >
-          <CustomRadio value="and">And</CustomRadio>
-          <CustomRadio value="or">Or</CustomRadio>
+          <CustomRadio value="_and">And</CustomRadio>
+          <CustomRadio value="_or">Or</CustomRadio>
         </RadioButtonGroup>
         <Box flexGrow={1}></Box>
         <Button size={'sm'} onClick={addFilter}>
@@ -70,35 +113,101 @@ const FilterGroup = ({ fields, filterGroup, setFilterGroup, onDelete }) => {
         <Button size={'sm'} onClick={addGroup}>
           Add Group
         </Button>
-        <IconButton isRound size={'sm'} icon={'delete'} onClick={onDelete} />
+        {!isRoot && (
+          <IconButton isRound size={'sm'} icon={'delete'} onClick={onDelete} />
+        )}
       </Stack>
-      {filters.map((f, index) =>
-        f.type === 'field' ? (
-          <FieldFilter
-            fields={fields}
-            filter={f}
-            setFilter={setFilter(index)}
-            key={index}
-            onDelete={() => deleteFilter(index)}
-          />
-        ) : (
-          <FilterGroup
-            fields={fields}
-            filterGroup={f}
-            setFilterGroup={setChildFilterGroup}
-            onDelete={() => deleteFilterGroup(index)}
-          />
-        )
-      )}
+      {filter?.filters?.length > 0 &&
+        filter.filters.map((f, index) =>
+          f.type === 'field' ? (
+            <FieldFilter
+              fields={fields}
+              filter={f}
+              setFilter={setFilterField(index)}
+              key={index}
+              onDelete={() => deleteFilter(index)}
+            />
+          ) : (
+            <FilterGroup
+              key={index}
+              fields={fields}
+              filter={f}
+              setFilter={setFilterGroup(index)}
+              onDelete={() => deleteFilterGroup(index)}
+            />
+          )
+        )}
     </Card>
   );
 };
 
-const constructFilters = (filterGroups) => {
-  return {};
+const constructFilters = (rootFilter) => {
+  const childFilters = rootFilter.filters
+    .filter(
+      (f) =>
+        (f.type === 'group' && f.filters.length > 0) ||
+        (f.type === 'field' && f.field && f.operator && f.value)
+    )
+    .map((f) => {
+      if (f.type === 'group') {
+        return constructFilters(f);
+      } else {
+        return { [f.field]: { [f.operator]: f.value } };
+      }
+    });
+  let result = {};
+  if (rootFilter.isNot) {
+    result._not = {
+      [rootFilter.groupType]: childFilters,
+    };
+  } else {
+    result = {
+      [rootFilter.groupType]: childFilters,
+    };
+  }
+  return result;
 };
 
-export default ({ schema = { fields: [] }, children }) => {
+const constructInitialFilters = (filter) => {
+  let result = {};
+  result.isNot = !!filter._not;
+  const actualFilter = result.isNot ? filter._not : filter;
+  if (actualFilter._and) {
+    result.groupType = '_and';
+    result.type = 'group';
+    result.filters = actualFilter._and.map(constructInitialFilters);
+  } else if (actualFilter._or) {
+    result.groupType = '_or';
+    result.type = 'group';
+    result.filters = actualFilter._or.map(constructInitialFilters);
+  } else {
+    result.type = 'field';
+    result.field = Object.keys(actualFilter)[0];
+    const details = actualFilter[result.field];
+    result.operator = Object.keys(details)[0];
+    result.value = details[result.operator];
+  }
+  return result;
+};
+
+export default ({
+  schema = { fields: [] },
+  where = {
+    _and: [
+      { status: { _eq: 'completed' } },
+      { status: { _eq: 'todo' } },
+      { priority: { _eq: 'high' } },
+      {
+        _not: {
+          _or: [{ status: { _eq: 'In Progress' } }, { name: { _eq: 'Task' } }],
+        },
+      },
+    ],
+  },
+  children,
+}) => {
+  const initialFilters = constructInitialFilters(where);
+  const [rootFilter, setRootFilter] = useState(initialFilters);
   const fields = schema?.fields
     ? schema.fields
         .filter((f) => f?.type?.name || f?.type?.ofType?.name)
@@ -108,20 +217,21 @@ export default ({ schema = { fields: [] }, children }) => {
         })
         .filter((f) => f.type && !f.name.startsWith('ref_'))
     : [];
-  const [filterGroups, setFilterGroups] = useState([{}]);
-  const addFilterGroup = () => {
-    setFilterGroups([...filterGroups, {}]);
-  };
+  const graphqlFilters = constructFilters(rootFilter);
   return (
     <Box>
-      <Stack>
-        {filterGroups.map((fg, index) => (
-          <FilterGroup key={index} filterGroup={fg} fields={fields} />
-        ))}
-        <Box></Box>
-        <Button onClick={addFilterGroup}>Add Group</Button>
-        <Box>{children(constructFilters(filterGroups))}</Box>
-      </Stack>
+      {fields.length > 0 && (
+        <Stack>
+          <FilterGroup
+            fields={fields}
+            isRoot={true}
+            filter={rootFilter}
+            setFilter={setRootFilter}
+          />
+          <Box></Box>
+          <Box>{children(graphqlFilters)}</Box>
+        </Stack>
+      )}
     </Box>
   );
 };
