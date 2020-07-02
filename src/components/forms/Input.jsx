f7a19d1cf3fe6c1951537d1edfeff196885f5169
Aravindh Sridharan <mail@aravindh.io>
2020-07-02 17:58:15 +0530

    Added auto updating input

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,6 +1,39 @@
-import { Input } from '@chakra-ui/core';
+import { Input, Select } from '@chakra-ui/core';
+import React from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
-export default ({ options, ...rest }) => {
-  const { control } = useFormContext(); // methods contain all useForm functions
-  return <Controller control={control} as={Input} size={'sm'} {...rest} />;
+import _ from 'lodash';
+import useMutation from 'src/hooks/graphql/useMutation';
+
+export default ({ name, options, ...rest }) => {
+  const delayedQuery = _.debounce((q) => update(q), 2000);
+  const { control, resource, id } = useFormContext(); // methods contain all useForm functions
+  const mutate = useMutation({ resource, operation: 'update' });
+  const update = (value) => {
+    if (id) {
+      mutate({
+        variables: {
+          object: { [name]: value },
+          where: { id: { _eq: id } },
+        },
+      });
+    }
+  };
+  return (
+    <Controller
+      control={control}
+      size={'sm'}
+      name={name}
+      as={({ onChange, value }) => (
+        <Input
+          size={'sm'}
+          value={value || ''}
+          {...rest}
+          onChange={(e) => {
+            delayedQuery(e.target.value);
+            onChange(e);
+          }}
+        />
+      )}
+    />
+  );
 };
