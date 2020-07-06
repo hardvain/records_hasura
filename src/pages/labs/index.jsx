52207f2265e5f4e590fd34fd89068b842f3fabad
Aravindh Sridharan <mail@aravindh.io>
2020-07-06 09:52:50 +0530

    Removing labs

--- src/pages/labs/index.jsx
+++
@@ -1,8 +0,0 @@
-import { Box } from '@chakra-ui/core';
-import ResourceSelector from 'src/components/forms/ResourceSelector';
-
-export default () => {
-  return <Box p={5}>
-    <ResourceSelector />
-  </Box>
-}
